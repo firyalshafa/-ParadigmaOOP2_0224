cd75ecf1f77ec9d8a9c490cb597fca55c9885c0c
firyalshafa <shafafiryal782@gmail.com>
2024-05-28 15:06:09 +0700

    membuat class orang

---
+++ polimorpi/polimorpi.cpp
@@ -0,0 +1,15 @@
+// polimorpi.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class seseorang {
+
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
