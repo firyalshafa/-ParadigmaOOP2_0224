55a256d5b8c08bfbbc248744cb8818acc13b48f3
firyalshafa <shafafiryal782@gmail.com>
2024-05-28 15:03:28 +0700

    membuat member inisialisasi budi

--- solvemultiwaris/solvemultiwaris.cpp
+++ solvemultiwaris/solvemultiwaris.cpp
@@ -35,11 +35,15 @@ public:
 };
 
 class budi : public pekerja, public pelajar {
+public:
 
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur)
+    {
+        cout << "budi dibuat\n" << endl;
+    }
 };
-
-
-
 
 int main()
 {
