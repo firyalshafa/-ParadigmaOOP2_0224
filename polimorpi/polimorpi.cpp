1669a54d84a252d427dede5401af8a909a6a8543
firyalshafa <shafafiryal782@gmail.com>
2024-05-28 15:08:34 +0700

    deklarasi metod  virtual dan implementasi fungsinya

--- polimorpi/polimorpi.cpp
+++ polimorpi/polimorpi.cpp
@@ -5,7 +5,11 @@
 using namespace std;
 
 class seseorang {
-
+public:
+    virtual void pesan() = 0;
+    //virtual void pesan(){
+    //cout << " pesan dari seseorang" << endl;
+    //}
 };
 
 int main()
