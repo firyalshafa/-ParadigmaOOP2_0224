e779c611712c2ec98990a5fa351a671693b66e32
firyalshafa <shafafiryal782@gmail.com>
2024-05-28 15:16:57 +0700

    memanggil fungsi utama

--- polimorpi/polimorpi.cpp
+++ polimorpi/polimorpi.cpp
@@ -32,6 +32,16 @@ public:
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+    //a.seseorang::pesan();
+    return 0;
+
 }
 
