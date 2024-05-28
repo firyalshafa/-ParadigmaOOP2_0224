8917d474cdc713d8f543cda733e4623a548e3164
firyalshafa <shafafiryal782@gmail.com>
2024-05-28 15:39:12 +0700

    membuat nooverriding

---
+++ noOverriding/noOverriding.cpp
@@ -0,0 +1,29 @@
+// noOverriding.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class baseclass  {
+public:
+    virtual void perkenalan() final {
+        cout << "halo saya function dari base class";
+    }
+};
+
+class derivedclass : public baseclass {
+public:
+    void perkenalan() {
+        cout << "hallo saya function dari derived class";
+    }
+};
+
+int main()
+{
+
+    derivedclass a;
+    a.perkenalan();
+
+    return 0;
+}
+
